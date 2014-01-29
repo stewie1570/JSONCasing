931f9fe6b40988abb806ac595ba0946e018e980f
Stewart Anderson <stewie1570@gmail.com>
2014-01-29 13:17:28 -0700

    Initial commit.

---
+++ JSONCasing.Tests/spec/JSONCasing.Tests.js
@@ -0,0 +1,58 @@
+﻿describe("JSONCasing object", function ()
+{
+    describe("Camel Case conversion", function ()
+    {
+        it("should support a shallow conversion", function ()
+        {
+            //Arrange
+            var pascalObj = { prop1: "prop 1", prop2: "prop 2" };
+            var camelObj = { Prop1: "prop 1", Prop2: "prop 2" };
+            actAndAssert(pascalObj, camelObj);
+        });
+
+        it("should support a deep conversion", function ()
+        {
+            //Arrange
+            var pascalObj = { prop1: "prop 1", obj1: { deepProp1: "dp1", deepProp2: "dp2" } };
+            var camelObj = { Prop1: "prop 1", Obj1: { DeepProp1: "dp1", DeepProp2: "dp2" } };
+            actAndAssert(pascalObj, camelObj);
+        });
+
+        function actAndAssert(pascalObj, camelObj)
+        {
+            //Act
+            var result = JSONCasing.toCamel(pascalObj);
+
+            //Assert
+            expect(JSON.stringify(result)).toBe(JSON.stringify(camelObj));
+        }
+    });
+
+    describe("Pascal Case conversion", function ()
+    {
+        it("should support a shallow conversion", function ()
+        {
+            //Arrange
+            var pascalObj = { prop1: "prop 1", prop2: "prop 2" };
+            var camelObj = { Prop1: "prop 1", Prop2: "prop 2" };
+            actAndAssert(pascalObj, camelObj);
+        });
+
+        it("should support a deep conversion", function ()
+        {
+            //Arrange
+            var pascalObj = { prop1: "prop 1", obj1: { deepProp1: "dp1", deepProp2: "dp2" } };
+            var camelObj = { Prop1: "prop 1", Obj1: { DeepProp1: "dp1", DeepProp2: "dp2" } };
+            actAndAssert(pascalObj, camelObj);
+        });
+
+        function actAndAssert(pascalObj, camelObj)
+        {
+            //Act
+            var result = JSONCasing.toPascal(camelObj);
+
+            //Assert
+            expect(JSON.stringify(result)).toBe(JSON.stringify(pascalObj));
+        }
+    });
+});
---
+++ JSONCasing/JSONCasing.js
@@ -0,0 +1,31 @@
+﻿JSONCasing = {
+    toCamel: function (obj)
+    {
+        return this.processObj(obj, this.toCamel, function (key) { return key.charAt(0).toUpperCase(); });
+    },
+
+    toPascal: function (obj)
+    {
+        return this.processObj(obj, this.toPascal, function (key) { return key.charAt(0).toLowerCase(); });
+    },
+
+    processObj: function (obj, caller, properFirstChar)
+    {
+        for (var key in obj)
+        {
+            if (typeof (obj[key]) == "object")
+                obj[key] = caller.call(this, obj[key]);
+            this.processObjectKey(obj, key, properFirstChar(key) + key.substr(1, key.length - 1));
+        }
+        return obj;
+    },
+
+    processObjectKey: function (obj, key, proper)
+    {
+        if (key != proper)
+        {
+            obj[proper] = obj[key];
+            delete obj[key];
+        }
+    }
+};
